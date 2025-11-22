e87b119d192a9e567818e7c7865ae35465c871d3
ToFuruta <tomohiro.9.10@icloud.com>
2025-11-23 01:40:58 +0900

    [add] ABC4425 A問題の解答

---
+++ ABC425/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  int sum = 0;
+  for (int i = 1; i <= N; i++){
+    if (i % 2 == 1) {
+      sum -= i * i * i;
+    }
+    else{
+      sum += i * i * i;
+    }
+  }
+  cout << sum;
+}
