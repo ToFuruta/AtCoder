4d494b58f088272cfcb6001799fbcf33c1a1f29d
ToFuruta <tomohiro.9.10@icloud.com>
2025-11-25 09:11:20 +0900

    [add] ABC419 A問題の解答

---
+++ ABC419/A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  string S;
+  cin >> S;
+
+  if (S == "red"){
+    cout << "SSS";
+  }
+  else if (S == "blue"){
+    cout << "FFF";
+  }
+  else if (S == "green"){
+    cout << "MMM";
+  }
+  else {
+    cout << "Unknown";
+  }
+}
