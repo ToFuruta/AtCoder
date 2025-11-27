1a59d4db335d78ca5a0c1d43271511e2853540c2
ToFuruta <tomohiro.9.10@icloud.com>
2025-11-28 01:04:18 +0900

    [fix] テンプレートの修正

--- tmp.cpp
+++ tmp.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 
 int main() {
 
-  return 0;
 }
