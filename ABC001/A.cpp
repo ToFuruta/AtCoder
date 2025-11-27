282e48a2516a4095a66b7c8cf9667db1e5db6396
ToFuruta <tomohiro.9.10@icloud.com>
2025-11-28 01:10:57 +0900

    [add] ABC001 A問題の解答

---
+++ ABC001/A.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int main() {
+  int h1, h2;
+  cin >> h1 >> h2;
+  cout << h1 - h2 << endl;
+}
