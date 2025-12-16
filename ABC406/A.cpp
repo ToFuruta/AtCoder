fcc44a7f9eb61c63caf5563b9d482ceb160bf198
ToFuruta <tomohiro.9.10@icloud.com>
2025-12-17 01:33:52 +0900

    [add] ABC406 A問題の解答

---
+++ ABC406/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  if (a > c || (a == c && b > d)) cout << "Yes";
+  else cout << "No";
+  return 0;
+}
